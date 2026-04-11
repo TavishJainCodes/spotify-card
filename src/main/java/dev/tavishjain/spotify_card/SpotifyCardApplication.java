01e9d5a6bd7c0768acaad0ad1ab41cfbaafd3637
NightMare <tavishjain.dev@gmail.com>
2026-04-11 12:57:12 +0530

    chore: add spring boot project structure

---
+++ src/main/java/dev/tavishjain/spotify_card/SpotifyCardApplication.java
@@ -0,0 +1,13 @@
+package dev.tavishjain.spotify_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tavishjain/spotify_card/SpotifyCardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tavishjain.spotify_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
