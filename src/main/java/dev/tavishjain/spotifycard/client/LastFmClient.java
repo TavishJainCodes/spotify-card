70dfe4f52eb3828ed76c7e40c42551cf10eab7a1
NightMare <tavishjain.dev@gmail.com>
2026-04-13 01:51:30 +0530

    feat: search-based card generation with artwork matching

--- src/main/java/dev/tavishjain/spotifycard/client/LastFmClient.java
+++ src/main/java/dev/tavishjain/spotifycard/client/LastFmClient.java
@@ -16,15 +16,31 @@ public class LastFmClient {
     public String getNowPlaying(String username) {
         RestTemplate restTemplate = new RestTemplate();
 
-        //https://ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=NightMare&api_key=abc123&format=json&limit=1
+        // https://ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=NightMare&api_key=abc123&format=json&limit=1
 
-        String url =
-            apiUrl +
-            "?method=user.getrecenttracks&user=" +
-            username +
-            "&api_key=" +
-            apiKey +
-            "&format=json&limit=1";
+        String url = apiUrl +
+                "?method=user.getrecenttracks&user=" +
+                username +
+                "&api_key=" +
+                apiKey +
+                "&format=json&limit=1";
+
+        String response = restTemplate.getForObject(url, String.class);
+        return response;
+    }
+
+    public String getInfo(String trackName) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // curl
+        // "https://ws.audioscrobbler.com/2.0?method=track.search&track=Kamaal+Hai&api_key=0cc55924034e77d0452f12ae19092d1a&format=json&limit=5"
+
+        String url = apiUrl +
+                "?method=track.search&track=" +
+                trackName +
+                "&api_key=" +
+                apiKey +
+                "&format=json&limit=10";
 
         String response = restTemplate.getForObject(url, String.class);
         return response;
